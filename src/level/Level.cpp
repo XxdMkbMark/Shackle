113939e9348c96bfff6007cb7997695e32d850ab
Qc9ff <95865734+Qc9ff@users.noreply.github.com>
2022-12-21 10:16:50 +0800

    Level crated

---
+++ src/level/Level.cpp
@@ -0,0 +1,11 @@
+//
+// Created by qu_a3 on 2022/12/21.
+//
+
+#include "Level.h"
+#include <array>
+using namespace std;
+
+namespace qcl {
+    enum BlockType {WALL, ROAD, START_POINT, DESTINATION};
+} // qcl
---
+++ src/level/Level.h
@@ -0,0 +1,11 @@
+//
+// Created by qu_a3 on 2022/12/21.
+//
+
+#ifndef SHACKLE_LEVEL_H
+#define SHACKLE_LEVEL_H
+
+namespace qcl {
+} // qcl
+
+#endif //SHACKLE_LEVEL_H
